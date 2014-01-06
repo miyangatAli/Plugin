10d493d57690ea2341f1b1168aa4481cb759a687
shgli <li_shugan@126.com>
2014-01-06 18:47:51 +0800

    make singleton thread-safe

--- plugin/include/Singleton.h
+++ plugin/include/Singleton.h
@@ -1,6 +1,7 @@
 #ifndef _SINGLETON_H
 #define _SINGLETON_H
 #include <boost/noncopyable.hpp>
+#include <boost/thread/once.hpp>
 
 template<typename T>
 class Singleton:boost::noncopyable
@@ -8,8 +9,22 @@ class Singleton:boost::noncopyable
 public:
     static T& Instance( void )
     {
-        static T instance;
-        return instance;
+        static boost::once_flag init_instance = BOOST_ONCE_INIT;
+        boost::call_once(init_instance,&Singleton::CreateInstance);
+
+        return *gpInstance;
     }
+
+private:
+    static void CreateInstance( void )
+    {
+        gpInstance = new T();
+    }
+
+    static T* gpInstance;
 };
+
+template<typename T>
+T* Singleton<T>::gpInstance = nullptr;
 #endif
+
